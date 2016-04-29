1860cebfad04f19ad93faee8fc1f85dadd33e8a9
darkfoxus <darkfoxus@gmail.com>
2016-04-29 09:03:52 -0500

    Second commit, including first header footer and the first deployiment of group real data
    Second commit, including first header footer and the first deployiment
    of group real data

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 $team [] = array (
 		'name' => 'Fernando Arango Isaza',
 		'mail' => 'farango@unal.edu.co',
-		'jobTitle' => 'Research Leader',
-		'desciption' => 'description',
-		'imgURL' => './images/demo/team-member2.jpg' 
+		'jobTitle' => 'Investigador Lider',
+		'desciption' => 'Profesor Titular - Universidad Nacional de Colombia, Facultad de Minas, Escuela de ciencias de la computación y de la decisión',
+		'imgURL' => './images/demo/team-member.gif' 
 );
 $team [] = array (
 		'name' => 'Danny Alvarez Eraso',
 		'mail' => 'daalvareze@unal.edu.co',
-		'jobTitle' => 'Researcher',
-		'desciption' => 'description',
-		'imgURL' => './images/demo/team-member2.jpg'
+		'jobTitle' => 'Investigador',
+		'desciption' => 'Estudiante de Maestría en Sistemas - Universidad Nacional de Colombia, Facultad de Minas, Escuela de ciencias de la computación y de la decisión',
+		'imgURL' => './images/danny.png'
 );
-$team [] = array (
+/*$team [] = array (
 		'name' => 'Danny Alvarez Eraso',
 		'jobTitle' => 'Researcher',
 		'desciption' => 'description',
@@ -30,7 +30,7 @@ $team [] = array (
 		'jobTitle' => 'Researcher',
 		'desciption' => 'description',
 		'imgURL' => './images/demo/team-member2.jpg'
-);
+);*/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="EN"
@@ -66,13 +66,6 @@ $team [] = array (
 	text-indent: -5000px;
 }
 </style>
-					<!--[if lte IE 6]><style type="text/css">#osfooter{position:absolute; display:none;}</style><![endif]-->
-					<!-- <script async="" src="//www.google-analytics.com/analytics.js"></script> -->
-					<!-- <script type="text/javascript" async="" src="./scripts/bsa.js"></script> -->
-					<!-- <script type="text/javascript" id="_bsap_js_2cdb89802e2deca5991138bb3e47b146" src="./scripts/s_2cdb89802e2deca5991138bb3e47b146.js" async="async"></script> -->
-					<!-- <script type="text/javascript" src="./scripts/pro.js" id="_bsap_premium_pro"></script> -->
-					<!-- <script type="text/javascript" id="_bsaPRO_js" src="./scripts/resc.js" async="async"></script> -->
-
 </head>
 
 <body id="top">
@@ -86,6 +79,17 @@ $team [] = array (
 					.getElementsByTagName('body')[0]).appendChild(bsa);
 		})();
 	</script>
+	
+	<div id="header">
+									                    
+                    <span>
+							<h1 id="logo" style="float: left;
+    color: #2A2A2A;
+    font-weight: bold;"><img style="padding-top:4px; padding-left:20px;" src="http://xue.medellin.unal.edu.co/seguridad/images/escudoUnal2.png"></span>
+								<br>Grupo de Investigación en Ingeniería de Software - GIIS
+							</h1><!-- END #logo -->
+</div>
+	
 	<div class="wrapper row1"></div>
 	<div class="wrapper row2">
 		<div id="container" class="clear">
@@ -98,42 +102,81 @@ $team [] = array (
 								class="imgholder" alt="Grupo de Investigacion en Ingenieria de Software">
 						
 						</div>
-						<p>Praesent imperdiet justo at sem. Nam accumsan dui eget
-							diam.Nulla quis ante eget pede fringilla rutrum. Proin mollis
-							tristique orci. Nam est. Fusce felis nisl, volutpat vel,
-							sollicitudin eu, luctus sit amet, sem.</p>
-						<p>Curabitur risus erat, scelerisque sit amet, mollis id,
-							consectetuer eu, neque. Curabitur sodales semper arcu. Sed
-							ullamcorper arcu eu ante. Vestibulum sed lacus. Quisque vitae
-							libero. Sed quis turpis vitae lectus vehicula tincidunt. Aliquam
-							quam mi, rutrum malesuada, consequat vitae, accumsan ut, lacus.
-							Nulla dictum vestibulum lorem.</p>
+						<p>El grupo tiene como plan el desarrollo de proyectos de investigación relacionados con el 
+						mejoramiento de la capacidad para desarrollar software de alta calidad con el mínimo posible 
+						de recursos. A este fin contribuyen las diferentes líneas de investigación de grupo, a saber:</p>
+						
+						<ol>
+							<li>La Arquitectura y Frameworks de Desarrollo WEB pretende tomar ventaja de una arquitectura 
+								modular, consistente yestable que facilite lograr la reutilización de código.</li>
+							<li>La Evolución de los modelos y del software apoya la capacidad del software de adaptarse 
+						  		de forma continua a los cambios de los requisitos en el área de aplicación. 
+						  </li>
+							<li>La Formalización y estandarización de la semántica de los modelos eliminan las 
+								  ambigüedades propias de las especificaciones de los lenguajes naturales y gráficos 
+								  garantizando una única interpretación de los modelos por todos los involucrados en 
+								  la construcción de la aplicación</li>
+							<li>La Generación Automática de código garantiza que el refinamiento de los modelos se lleve 
+								  a cabo manteniendo, a lo largo del ciclo de vida del software, la semántica de los requisitos 
+								  previamente elicitados,</li>
+							<li>La Ingeniería de líneas de productos de software garantiza la adaptabilidad del software 
+						  		previamente elaborado ante los cambios de los requisitos en un área de aplicación específica</li>
+							<li>El Modelamiento en el desarrollo de software, garantiza la valides de los requisitos elicitados 
+						 		frente a las necesidades reales del área de aplicación.</li>
+							<li>La Optimización de Código a nivel Fuente 
+								 se orienta a la producción de código ejecutable que tome ventaja de los avances en las tecnologías 
+								 de procesamiento en multi-nucleo y multi-equipo en redes distribuidas. </li>
+							<li>La Seguridad del software 
+						 en la WEB apoya la producción de software resistente a ataques o a uso malicioso que puedan hacer de él, 
+						 usuarios internos o externos a la organización. </li>
+						</ol>
+						
+						<p>Para impulsar las líneas se pretende realizar actividades que se describen a continuación:
+						
+						<ul>
+							<li>Mantener Semilleros de investigación para fomentar la proyección del estado del arte: El desarrollo 
+						de software en el mundo, y en particular en Colombia, están marcadas por el uso de múltiples tecnologías 
+						heterogéneas amalgamadas de forma arbitraria causando grandes problemas de calidad, costo y mantenimiento. 
+						Esto es, el uso de múltiples lenguajes hace complejo y costosa la construcción de piezas de software que 
+						dé respuesta a las necesidades del mundo actual.</li>
+							<li><p>Para mantener la consistencia entre los diferentes lenguajes actualmente usados en las fases del ciclo de vida 
+						del software, es necesario primero contar con una semántica uniforme y precisa en un lenguaje formal. 
+						Esta semántica apoyará la consistencia entre los modelos de las diferentes fases del ciclo de vida del 
+						software, apoyando la modificación, el refinamiento, y en últimas la generación automática de código.</p> 
+						
+						<p>En particular el refinamiento del modelo de clases al modelo relacional se enfrenta al problema descrito 
+						como desacoplo de impedancia (impedance missmatch). Este problema consiste en llevar los datos de las 
+						aplicaciones orientadas a objetos a las bases de datos relacionales y viceversa. 
+						Este problema existe en casi todo tipo de herramientas de desarrollo usadas actualmente, en particular, 
+						por los Frameworks para el desarrollo Web (WAFs). 
+						El estado actual de la generación automática de código desde las especificaciones de análisis no permite 
+						producir aplicaciones lo suficientemente versátiles para adaptarse a los cambios constantes en los 
+						requisitos del área de aplicación. Esto es causado por la informalidad de las técnicas actuales que se 
+						apoyan más en la intuición propia de quien construye la herramienta que en una base teórica que dé cuenta 
+						de los requisitos y reglas que deben cumplir todos los generadores de código. </p></li>
+						</ul> 
 					</div>
 
 					<div id="skillset" class="clear">
 						<div class="fl_left first">
-							<h2>Vivamuslibero Auguer</h2>
-							<p>Lorem ipsum dolor sit amet, consectetaur adipisicing elit, sed
-								do eiusmod tempor incididunt ut labore et dolore magna aliqua ut
-								enim ad minim veniam.</p>
-							<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa
-								qui officia deserunt mollit anim id est laborum.</p>
+							<h2>Productos de Investigación</h2>
+							<p>Nuestro grupo de investigación cuenta con más de 130 productos en la base de datos de
+							 Colciencias, es un grupo maduro y preocupado por apoyar la investigación a través de 
+							 la participación constante en congresos a nivel nacional e internacional y de la 
+							 producción de artículos y elementos didácticos que apoyan la educación.</p>
 						</div>
 						<div class="fl_left">
-							<h2>Vivamuslibero Auguer</h2>
+							<h2>Links de interés</h2>
 							<ol>
-								<li><a href="http://www.os-templates.com/page-templates">Aliquam
-										venenatis leo et orci.</a></li>
-								<li><a href="http://www.os-templates.com/page-templates">Vivamus
-										eleifend sollicitudin eros.</a></li>
-								<li><a href="http://www.os-templates.com/page-templates">Maecenas
-										vitae nunc.</a></li>
-								<li><a href="http://www.os-templates.com/page-templates">Ut
-										pretium odio eu nisi.</a></li>
-								<li><a href="http://www.os-templates.com/page-templates">Nam
-										condimentum mi id magna.</a></li>
-								<li><a href="http://www.os-templates.com/page-templates">Pellentesque
-										consectetuer, felis vel.</a></li>
+								<li><a href="http://scienti.colciencias.gov.co:8080/gruplac/jsp/visualiza/visualizagr.jsp?nro=00000000003899">
+									Información del grupo en la plataforma GrupLac de Colciencias</a></li>
+								<li><a href="http://xue.medellin.unal.edu.co/seguridad/">
+									Foro de Seguridad Informática</a></li>
+								  <li><a href="http://xue.medellin.unal.edu.co/grupois/wiki/">
+									Wiki para el aprendizaje de Frameworks Web</a></li>
+								<li><a href="http://xue.medellin.unal.edu.co/farango/">
+									Prueba de concepto del PISIS Framework - Generación Atomática de Vistas</a></li>
+								
 							</ol>
 						</div>
 					</div>
@@ -160,67 +203,15 @@ $team [] = array (
 							</div>
 						</li>
 					<?php }	?>
-						<li>
-							<div class="figure clear">
-								<img src="images/demo/team-member.gif" alt="Template Demo Image">
-									<div class="figcaption">
-										<p class="team_name">Name Goes Here</p>
-										<p class="team_title">Job Title Here</p>
-										<p class="team_description">Vestassapede et donec ut est
-											liberos sus et eget sed eget. Quisqueta habitur augue magnisl
-											magna phas ellus.</p>
-									</div>
-							
-							</div>
-						</li>
-						<li>
-							<div class="figure clear">
-								<img src="images/demo/team-member.gif" alt="Template Demo Image">
-									<div class="figcaption">
-										<p class="team_name">Name Goes Here</p>
-										<p class="team_title">Job Title Here</p>
-										<p class="team_description">Vestassapede et donec ut est
-											liberos sus et eget sed eget. Quisqueta habitur augue magnisl
-											magna phas ellus.</p>
-									</div>
-							
-							</div>
-						</li>
-						<li>
-							<div class="figure clear">
-								<img src="images/demo/team-member.gif" alt="Template Demo Image">
-									<div class="figcaption">
-										<p class="team_name">Name Goes Here</p>
-										<p class="team_title">Job Title Here</p>
-										<p class="team_description">Vestassapede et donec ut est
-											liberos sus et eget sed eget. Quisqueta habitur augue magnisl
-											magna phas ellus.</p>
-									</div>
-							
-							</div>
-						</li>
-						<li>
-							<div class="figure clear">
-								<img src="images/demo/team-member.gif" alt="Template Demo Image">
-									<div class="figcaption">
-										<p class="team_name">Name Goes Here</p>
-										<p class="team_title">Job Title Here</p>
-										<p class="team_description">Vestassapede et donec ut est
-											liberos sus et eget sed eget. Quisqueta habitur augue magnisl
-											magna phas ellus.</p>
-									</div>
-							
-							</div>
-						</li>
 					</ul>
 				</div>
-
 			</div>
 
-
-
-
 		</div>
+	</div>
+	
+	<div id="footer">
+	
 	</div>
 </body>
 </html>
